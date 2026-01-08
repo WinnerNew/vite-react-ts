a314c96dcd37aff34823afac216afc5e39991d99
liuwenbin <775405088@qq.com>
2026-01-08 15:51:42 +0800

    fix: test

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -64,7 +64,7 @@ export default function RecipeReviewCard() {
         <Typography variant="body2" color="text.secondary">
           This impressive paella is a perfect party dish and a fun meal to cook
           together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like. 0
+          mussels, if you like.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
